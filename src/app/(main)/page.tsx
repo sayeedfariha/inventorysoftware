ae25aa91d52314e531c6ca94b4b291c1b0c0290b
sayeedfariha <sayeedfariha79@gmail.com>
2024-10-23 22:54:56 +0600

    first

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,23 +1,27 @@
 import Image from "next/image";
+import { CreditCard, Activity, BarChart2, PieChart } from 'lucide-react';
+import React from 'react';
+import { Bar } from "react-chartjs-2";
+import {Chart as ChartJS}from "react-chartjs-2";
 
-
-
-
- export default function Home() {
+export default function Dashboard() {
    return (
   <div className="w-full grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-slate-50">
-    <div className="flex">
-      <h2>Sales Activity</h2>
-      <div className="grid grid-cols-4">
-       <div className="rounded-lg border-slate-100 bg-slate-200 hover:border-blue-400 bg-white p-8">
-        <h4>10</h4>
-        <small>Qty</small>
-        <div className="flex"></div>
-       </div>
-      </div>
+    
+  <div className="bg-blue-100 border-b borde-slate-300 p-8">
+   <h2>Sales Over View</h2>
+
+
+
+
+
+
+
+     </div>
     </div>
-    </div> 
+
   );
  }
+
 
 
